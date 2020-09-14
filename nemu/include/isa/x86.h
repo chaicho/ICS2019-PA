eead19250709edc88001d06e286aee588124a2f6
tracer-ics2020 <tracer@njuics.org>
2020-09-15 01:20:28 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 3a8fba7ba88d4d0588bef2918589c23a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-42-generic       Architecture: x86-64  01:20:28 up  3:47,  1 user,  load average: 1.03, 1.32, 1.37

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -18,7 +18,7 @@
  */
 
 typedef struct {
-union {
+
   union{
     uint32_t _32;
     uint16_t _16;
@@ -30,9 +30,9 @@ union {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */ 
-   
-    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-  };
+  struct 
+  {   
+    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
 
   
   vaddr_t pc;
