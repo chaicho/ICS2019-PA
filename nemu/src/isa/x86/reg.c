577cc503adb36bbd8dbd0c080c6d34742d8729b5
tracer-ics2020 <tracer@njuics.org>
2020-09-16 00:20:34 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: bff1bd7cb760436088148199d3550e57   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-42-generic       Architecture: x86-64  00:20:34 up  9:33,  1 user,  load average: 0.60, 0.53, 0.59

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -44,6 +44,7 @@ void reg_test() {
 void isa_reg_display() {
   int i;
   for (i = 0; i <= 7; i ++) {
+        printf ("uiuuu\n");
        printf("%s : %8x    ",regsb[i],reg_b(i));
        printf("%s : %8x    ",regsw[i],reg_w(i));
        printf("%s : %8x    ",regsl[i],reg_l(i)); 
