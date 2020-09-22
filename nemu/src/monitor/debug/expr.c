e763278b16e168a6522b6202483829617792c67d
tracer-ics2020 <tracer@njuics.org>
2020-09-22 23:37:59 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 2c627e6d79234171ba1a4235ed8b3496   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  23:37:59 up 12 min,  1 user,  load average: 0.61, 0.45, 0.30

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -71,6 +71,8 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
+              printf("%s\n",rules[i].regex);
+
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
