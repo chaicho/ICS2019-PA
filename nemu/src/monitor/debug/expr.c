de5da6a627a4388f5af4cd9fdbda95b7f11eca8a
tracer-ics2020 <tracer@njuics.org>
2020-09-23 09:51:01 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 0b2c702ee6b1472fa4cf9e56346df300   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  09:51:01 up 7 min,  1 user,  load average: 0.95, 0.99, 0.55

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -207,6 +207,6 @@ word_t expr(char *e, bool *success) {
   }
   
   /* TODO: Insert codes to evaluate the expression. */
-  printf("%d\n",nr_token);
+ // printf("%d\n",nr_token);
   return eval(0,nr_token-1);
 }
