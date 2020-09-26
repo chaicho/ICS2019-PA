da118c6d602797c98fe99572b30661bfdd8718c5
tracer-ics2020 <tracer@njuics.org>
2020-09-26 15:58:44 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 251bfb4f9ed047fa8c8bb35ac842bd5e   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  15:58:44 up  4:16,  1 user,  load average: 1.72, 1.19, 1.17

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -4,12 +4,12 @@ void init_device();
 void engine_start() {
   /* Initialize devices. */
   init_device();
-
-  /* Receive commands from user. */
-  ui_mainloop();
   FILE *fp = fopen("/home/chaicho/ics2020/nemu/tools/gen-expr/input", "r");
   char str[50];
   while(fgets(str,40,fp)!=NULL){ 
     puts(str);
   }
+  /* Receive commands from user. */
+  ui_mainloop();
+
 }
