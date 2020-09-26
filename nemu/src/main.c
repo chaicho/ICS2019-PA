1c4a48ccc39418f7504cea54c8ad0e3206f5ec1a
tracer-ics2020 <tracer@njuics.org>
2020-09-26 12:53:46 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 251bfb4f9ed047fa8c8bb35ac842bd5e   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  12:53:46 up  1:11,  1 user,  load average: 0.86, 1.20, 1.29

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,13 +5,13 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
-
-  /* Start engine. */
-  engine_start();
   FILE *fp = fopen("/home/chaicho/ics2020/nemu/tools/gen-expr/input", "r");
   char str[50];
   if(fgets(str,40,fp)!=NULL){ 
   puts(str);
   }
+  /* Start engine. */
+  engine_start();
+ 
   return is_exit_status_bad();
 }
