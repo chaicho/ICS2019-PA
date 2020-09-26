6896a8fe0819bebc24ebb3e19ec225e3b8ddfb03
tracer-ics2020 <tracer@njuics.org>
2020-09-26 17:28:21 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 251bfb4f9ed047fa8c8bb35ac842bd5e   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  17:28:21 up  5:46,  1 user,  load average: 2.76, 2.15, 1.65

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -1,14 +1,9 @@
 void ui_mainloop();
 void init_device();
-#include<stdio.h>
 void engine_start() {
   /* Initialize devices. */
   init_device();
-  FILE *fp = fopen("/home/chaicho/ics2020/nemu/tools/gen-expr/input", "r");
-  char str[50];
-  while(fgets(str,40,fp)!=NULL){ 
-    puts(str);
-  }
+
   /* Receive commands from user. */
   ui_mainloop();
 
--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,12 +2,27 @@ void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"monitor/debug/expr.h"
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   init_monitor(argc, argv);
 
   /* Start engine. */
   engine_start();
-   
+  FILE *fp = fopen("/home/chaicho/ics2020/nemu/tools/gen-expr/input", "r");
+  char str[50];
+  while(fgets(str,40,fp)!=NULL){ 
+     char *cmd = strtok(str, " ");
+    if (cmd == NULL) { 
+      continue; 
+    }
+   char *args = cmd + strlen(cmd) + 1;
+   bool success=true;
+   unsigned x=expr(args,&success);
+   if(x==(unsigned)atoi(cmd)) printf("You are right!\n");
+   else printf("Right:%u,Wrong:%u\n",atoi(cmd),x);
+  }
   return is_exit_status_bad();
 }
