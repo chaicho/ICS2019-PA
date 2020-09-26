49610af4c66972af0774cea02af926c57859862e
tracer-ics2020 <tracer@njuics.org>
2020-09-26 15:57:16 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 251bfb4f9ed047fa8c8bb35ac842bd5e   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  15:57:16 up  4:14,  1 user,  load average: 0.90, 0.97, 1.11

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -10,29 +10,34 @@ static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
+"#include <stdint.h>\n"
+"#include <assert.h>\n"
 "int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
+"  unsigned tmp= %s;"
+//  unsigned tmp=(unsigned) result;"
+//" if(result==tmp){"
+"  printf(\"%%u\", tmp); "
+//"  else assert(0);"
 "  return 0; "
 "}";
-int ii,j,tmp;
+int ii,j,tmp,flag=0;
 static inline void gen_num(){
-     tmp=rand()%4+1;
+     tmp=rand()%6+1;
      j=0;
      buf[ii++]=rand()%8+1+'0';
-    for(j=1;j<=tmp+3;j++) {
+    for(j=1;j<=tmp;j++) {
       buf[ii++]=rand()%9+'0';
       }
 }
 static inline void gen_rand_expr() {
   if(ii>=500) {
-
+    gen_num();
     return;
     }
   switch (rand()%3)
   {
   case 0:
-     gen_num();
+    gen_num();
     break;
   case 1:
     buf[ii++]='(';
@@ -75,24 +80,24 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     ii=0; 
-    memset(buf,0,1000);
+    memset(buf,0,65535);
      gen_rand_expr();
-
+    flag=1;
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
-
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
-
+    int ret = system("gcc /tmp/.code.c -Wall -Werror -o  /tmp/.expr");
+    if (ret != 0)  {
+      i--;
+      continue;
+    }
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
-
     int result;
-    fscanf(fp, "%d", &result);
+    fscanf(fp, "%u", &result);
     pclose(fp);
 
     printf("%u %s\n", result, buf);
