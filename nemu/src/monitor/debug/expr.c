155f1aaaff04bd2e1605f947a7b588f774e3b7ec
tracer-ics2020 <tracer@njuics.org>
2020-09-30 08:14:56 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d585b104926f4418bf1660dc935427b2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  08:14:56 up 26 min,  1 user,  load average: 1.21, 0.92, 0.60

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -59,7 +59,7 @@ typedef struct token {
   char str[32];//32
 } Token;
 
-static Token tokens[60] __attribute__((used)) = {}; //存放正则表达式
+static Token tokens[120] __attribute__((used)) = {}; //存放正则表达式
 static int nr_token __attribute__((used))  = 0; //正则表达式的的数量
  bool address=false;
  int lowest=-1;// 用来记录当前优先级最低的符号
@@ -275,7 +275,7 @@ unsigned eval(int p,int q){
    return 0;
 }
 word_t expr(char *e, bool *success) {
-  memset(tokens,0,60*sizeof(Token));
+  memset(tokens,0,120*sizeof(Token));
   if (!make_token(e)) {
     *success = false;
     return 0;
