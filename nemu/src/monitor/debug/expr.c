5fbe1a0f05c08057ce14a726f4788471b989a17a
tracer-ics2020 <tracer@njuics.org>
2020-09-30 08:50:46 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d585b104926f4418bf1660dc935427b2   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  08:50:46 up  1:02,  1 user,  load average: 0.86, 0.52, 0.40

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,11 +204,12 @@ unsigned eval(int p,int q){
               lef--;
           }
           else if(lef==0){
-              printf("at %d\n with lowest %d\n",i,lowest);
+             
                if(priority[tokens[i].type]>=lowest){
                 if(!(i>0&&tokens[i].type==TK_NEG&&tokens[i-1].type==TK_NEG)) record=i;
                  lowest=priority[tokens[i].type];
-               }
+                 printf("at %d\n with lowest %d\n",i,lowest);
+               } 
           }
          // printf("%d\n",lef);
       }
