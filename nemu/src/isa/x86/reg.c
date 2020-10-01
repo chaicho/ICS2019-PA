020f95a83e1b65b2dcd6b50de9f1e2a1acb0c499
tracer-ics2020 <tracer@njuics.org>
2020-10-01 14:05:16 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d379b3ade8d74fbbb08a2f29ddd64b59   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  14:05:16 up 1 day,  4:11,  1 user,  load average: 1.45, 1.36, 1.14

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -79,15 +79,15 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   *success=true;    
   int ww=0;
   for(ww=0;ww<7;ww++){
-    if(strcmp(s,regsl[ww])){
+    if(!strcmp(s,regsl[ww])){
       printf("s= %s %s\n",s,regsl[ww]);
       return reg_l(ww);
       }
-    if(strcmp(s,regsb[ww])){
+    if(!strcmp(s,regsb[ww])){
          printf("%s %s\n",s,regsb[ww]);
          return  reg_b(ww);
     }
-    if(strcmp(s,regsw[ww])) {
+    if(!strcmp(s,regsw[ww])) {
          printf("%s %s\n",s,regsw[ww]);
          return reg_w(ww);    
      }
