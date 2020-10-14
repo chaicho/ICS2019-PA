6b0d9addd5ad65348c328433817ea173625b4023
tracer-ics2020 <tracer@njuics.org>
2020-10-14 16:43:40 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 9c6247e91cae48048181e1e88bf7b6ac   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  16:43:40 up 32 min,  1 user,  load average: 1.01, 0.92, 0.83

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -18,7 +18,7 @@ static inline void cmp_internal(DecodeExecState *s) {
 
 static inline def_EHelper(sub) {
  // rtl_sub(s,ddest,ddest,dsrc1);
- // TODO();
+ TODO();
 }
 
 static inline def_EHelper(cmp) {
