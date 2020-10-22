7ae16dcf19ae0792d0ab821d69a89e64163435d8
tracer-ics2020 <tracer@njuics.org>
2020-10-22 17:09:03 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 0b2d4d130cf14b6b9f65c6834994b084   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  17:09:03 up  1:03,  1 user,  load average: 1.17, 1.11, 0.85

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -27,7 +27,8 @@ static inline def_rtl(sr, int r, const rtlreg_t* src1, int width) {
 static inline def_rtl(push, const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-    rtl_subi(s,&cpu.esp,&cpu.esp,4);
+    rtl_subi(s,t0,&cpu.esp,4);
+    cpu.esp=*t0;
     rtl_sm (s,&cpu.esp, 0,src1,4);
 }
 
