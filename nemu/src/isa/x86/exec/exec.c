2d9aa4479745108d75f302b42a822dcf21020dc0
tracer-ics2020 <tracer@njuics.org>
2020-10-23 00:15:34 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 43f76d26e4614161a111572060de6f55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  00:15:34 up  4:34,  1 user,  load average: 0.79, 0.68, 0.69

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -139,9 +139,11 @@ again:
     IDEX (0x58, r,pop)
     IDEX (0x8d, lea_M2G ,lea)
     IDEXW (0x84,G2E, test,1)
+    IDEXW (0xeb,J,jmp,1)
     IDEX (0x3b, E2G , cmp)
     EX( 0x47,inc)
    // EX (0x5d,pop)
+  
     EX   (0xc3, ret )
   case 0x66: s->isa.is_operand_size_16 = true; goto again;
   default: exec_inv(s);
