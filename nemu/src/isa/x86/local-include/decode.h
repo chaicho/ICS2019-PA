40c799e170135aea12f803aa934fe135f90ccdcb
tracer-ics2020 <tracer@njuics.org>
2020-10-23 00:39:08 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 43f76d26e4614161a111572060de6f55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  00:39:08 up  4:57,  1 user,  load average: 0.85, 0.87, 0.85

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -49,7 +49,7 @@ static inline def_DopHelper(SI) {
   assert(op->width == 1 || op->width == 4);
   sword_t simm=instr_fetch(&s->seq_pc,op->width);
   if(op->width==1) simm=simm|0xffffff00;
-  printf("simm:%d\n",simm);
+  //printf("simm:%d\n",simm);
   operand_imm(s,op,load_val,simm,op->width);  
   //imm和simm是union起来的，所以不需要专门写函数
   //if(op->width==1) op->imm=(signed)
@@ -268,6 +268,7 @@ static inline def_DHelper(a2O) {
 static inline def_DHelper(J) {
   decode_op_SI(s, id_dest, false);
   // the target address can be computed in the decode stage
+  printf("%d",id_dest->simm);
   s->jmp_pc = id_dest->simm + cpu.pc;
 }
 
