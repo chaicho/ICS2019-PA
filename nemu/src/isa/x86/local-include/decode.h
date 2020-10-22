4fddb3d10d3304665d092e2c7dcbd0f15a5c94ca
tracer-ics2020 <tracer@njuics.org>
2020-10-23 01:20:01 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 43f76d26e4614161a111572060de6f55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:20:01 up  5:38,  1 user,  load average: 0.89, 1.00, 0.87

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -269,7 +269,7 @@ static inline def_DHelper(J) {
   decode_op_SI(s, id_dest, false);
   // the target address can be computed in the decode stage
   printf("%d\n",id_dest->simm);
-  s->jmp_pc = id_dest->simm + s->seq_pc;
+  s->jmp_pc = id_dest->simm + cpu.pc;
 }
 
 static inline def_DHelper(push_SI) {
