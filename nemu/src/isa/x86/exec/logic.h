9d493673a2cd221ce8cb9a1aa6d5d67f60f0583d
tracer-ics2020 <tracer@njuics.org>
2020-10-24 01:21:00 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:21:00 up  5:39,  1 user,  load average: 2.77, 1.25, 0.87

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -14,19 +14,11 @@ static inline def_EHelper(test) {
 static inline def_EHelper(and) {
   //TODO();
   printf("OPTYPE : dest: %d   src1 : %d\n",s->dest.type,s->src1.type);
-  if(s->src1.width==1&&s->src1.type==OP_TYPE_IMM) rtl_sext(s,dsrc1,dsrc1,4); 
-  // printf("%ux\n",*s0);
-  printf( " dest :%x  src1:  %x\n", *ddest, *dsrc1 );
-  rtl_and(s,s0,ddest,dsrc1);
-  printf("The truth is %x\n",*ddest);
-  *ddest=*s0;
-  //rtl_mv(s,ddest,s0);
-  //if(ddest==&) assert(0);
-  //reg_b(0)=*s0;
-  // printf("%x compared with ",*ddest); 
- // printf("TYPE : %d\n",id_dest->type );
- // operand_write(s,id_dest,s2);
-   //printf("%x\n",*ddest);
+  if(s->src1.width==1&&s->src1.type==OP_TYPE_IMM) rtl_sext(s,s1,dsrc1,4); 
+  else *s1=*dsrc1;
+  rtl_and(s,s0,ddest,s1);
+  operand_write(s,id_dest,s0);
+
   cpu.eflag.CF=0;
   cpu.eflag.OF=0;
   rtl_update_ZFSF(s,ddest,s->width);
