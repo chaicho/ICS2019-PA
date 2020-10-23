ca78d34385bb8d13c95e21e3c823207423abc082
tracer-ics2020 <tracer@njuics.org>
2020-10-24 01:39:41 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:39:41 up  5:57,  1 user,  load average: 1.44, 1.60, 1.33

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,6 +4,34 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  if(ref_r->eax!=cpu.eax) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->eax,cpu.eax);
+    return false;
+  }
+   if(ref_r->ebp!=cpu.ebp) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->ebp,cpu.ebp);
+    return false;
+  }
+   if(ref_r->ebx!=cpu.ebx) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->ebx,cpu.ebx);
+    return false;
+  }
+   if(ref_r->ecx!=cpu.ecx) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->ecx,cpu.ecx);
+    return false;
+  }
+   if(ref_r->edi!=cpu.edi) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->edi,cpu.edi);
+    return false;
+  }
+   if(ref_r->edx!=cpu.edx) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->edx,cpu.edx);
+    return false;
+  }
+  if(ref_r->esp!=cpu.esp) {
+    printf("The supposed value: %x \n Wrong value : %x\n ",ref_r->esp,cpu.esp);
+    return false;
+  }
   return ref_r->eax==cpu.eax&&ref_r->ebp==cpu.ebp&&ref_r->ebx==cpu.ebx&&ref_r->ecx==cpu.ecx&&ref_r->edi==cpu.edi&&ref_r->edx==cpu.edx&&ref_r->esp==cpu.esp;
 }
 
