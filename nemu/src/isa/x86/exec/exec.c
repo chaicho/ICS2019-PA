47c9bdf749fa7d47fb4e8a6317a9b755cbc6be0d
tracer-ics2020 <tracer@njuics.org>
2020-10-23 15:38:25 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  15:38:25 up 14:51,  1 user,  load average: 1.29, 1.23, 1.19

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -71,7 +71,7 @@ static inline def_EHelper(2byte_esc) {
   switch (opcode) {
   /* TODO: Add more instructions!!! */
     IDEX (0x01, gp7_E, gp7)
-    IDEX (0X94,setcc_E, setcc)
+    IDEXW (0X94,setcc_E, setcc,1)
     IDEX (0X95,setcc_E, setcc)
     IDEX (0xb6,mov_E2G,movsx)
 
