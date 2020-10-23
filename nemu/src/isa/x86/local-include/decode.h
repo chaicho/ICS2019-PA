b08bb2ae2eee6fce1e17dae7ec24e40f384f5df3
tracer-ics2020 <tracer@njuics.org>
2020-10-23 14:03:34 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  14:03:34 up 13:16,  1 user,  load average: 0.33, 0.76, 1.11

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -207,7 +207,7 @@ static inline def_DHelper(SI2E) {
   assert(id_dest->width == 2 || id_dest->width == 4);
   operand_rm(s, id_dest, true, NULL, false);
   printf("%x\n",*ddest);
-  id_src1->width = 1;
+  // /id_src1->width = 1;
   decode_op_SI(s, id_src1, true);
   if (id_dest->width == 2) {
     *dsrc1 &= 0xffff;
