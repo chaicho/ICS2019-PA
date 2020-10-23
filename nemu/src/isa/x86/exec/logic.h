a83b997aa9e74bfc097341bd08d0c83eaeac4f1e
tracer-ics2020 <tracer@njuics.org>
2020-10-24 01:48:11 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:48:11 up  6:06,  1 user,  load average: 1.47, 1.33, 1.29

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -14,12 +14,13 @@ static inline def_EHelper(test) {
 static inline def_EHelper(and) {
   //TODO();
   //printf("OPTYPE : dest: %d   src1 : %d\n",s->dest.type,s->src1.type);
+  id_src1->width = s->isa.is_operand_size_16 ? 2 : 4;
+  id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
   if(s->src1.width==1) *dsrc1|=0xfffffff00;
   else if(s->src1.width==2) *dsrc1|=0xffff0000; 
   printf("sc1: %x\n",*dsrc1);
   rtl_and(s,s0,ddest,dsrc1);
-  id_src1->width = s->isa.is_operand_size_16 ? 2 : 4;
-  id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
+  
   operand_write(s,id_dest,s0);
   cpu.eflag.CF=0;
   cpu.eflag.OF=0;
