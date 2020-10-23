6257f05216eb31a088a2529247a6b18299c572ea
tracer-ics2020 <tracer@njuics.org>
2020-10-23 14:08:10 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  14:08:10 up 13:20,  1 user,  load average: 0.76, 0.81, 1.04

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -48,7 +48,7 @@ static inline def_DopHelper(I) {
 static inline def_DopHelper(SI) {
   assert(op->width == 1 || op->width == 4);
   sword_t simm=instr_fetch(&s->seq_pc,op->width);
-   // printf("ddest: %x\n",*ddest);
+    printf("ddest: %x\n",*ddest);
   *s0=simm;
   //printf("simm:%x\n",*s0);
   if(op->width==1) rtl_sext(s,s0,s0,4);
@@ -209,8 +209,9 @@ static inline def_DHelper(SI2E) {
   operand_rm(s, id_dest, true, NULL, false);
  
   id_src1->width = 1; 
+  printf("ddest: %x\n",*ddest);
   decode_op_SI(s, id_src1, true); 
- 
+   printf("ddest: %x\n",*ddest);
   if (id_dest->width == 2) {
     *dsrc1 &= 0xffff;
   }
