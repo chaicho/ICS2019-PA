5b25438618f3fc0a1bab8775af54b14104fba955
tracer-ics2020 <tracer@njuics.org>
2020-10-23 14:19:29 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  14:19:29 up 13:32,  1 user,  load average: 1.44, 1.01, 0.93

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -33,9 +33,9 @@ static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   rtl_shli(s,t0,src1,8*(width-s->src1.width));
   //assert(0);q
 //  printf("from :%x\n",*t0);
-  rtl_sari(s,ddest,t0,8*(width-s->src1.width));
+  rtl_sari(s,dest,t0,8*(width-s->src1.width));
   //operand_write(s,id_dest,t0);
-  printf("to: %x\n",*ddest);
+ // printf("to: %x\n",*dest);
  // rtl_sari(s,t0,t0,8*(width-id_src1->width));
   //TODO();
   //operand_write(s,id_dest,src1);
