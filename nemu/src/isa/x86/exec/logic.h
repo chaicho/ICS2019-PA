84c76e7ac676720cd1657273c5255ce8667f65bd
tracer-ics2020 <tracer@njuics.org>
2020-10-23 13:20:28 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  13:20:29 up 12:33,  1 user,  load average: 2.05, 1.77, 1.57

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -13,8 +13,8 @@ static inline def_EHelper(test) {
 
 static inline def_EHelper(and) {
  //TODO();
-  if(s->src1.width==1) rtl_sext(s,ddest,dsrc1,4); 
-  printf("%x\n",*ddest);
+  if(s->src1.width==1) rtl_sext(s,dsrc1,dsrc1,4); 
+  printf("%x\n",*dsrc1);
   rtl_and(s,ddest,ddest,dsrc1);
   //operand_write(s,id_dest,t0);
   cpu.eflag.CF=0;
