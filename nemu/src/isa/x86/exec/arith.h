d8bd63a76666c9854aeb9b005f508bde8aa90bb4
tracer-ics2020 <tracer@njuics.org>
2020-10-23 14:08:57 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: e2937b3e7316499981693d813baad452   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  14:08:57 up 13:21,  1 user,  load average: 0.68, 0.79, 1.02

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -36,7 +36,7 @@ static inline def_EHelper(sub) {
  // rtl_add(s, s0, dsrc1, s0);
   printf("%x ---- %x\n",*ddest,*dsrc1);
   rtl_sub(s, s1, ddest, dsrc1);
-  printf("%x\n",*s1);
+  //printf("%x\n",*s1);
   rtl_update_ZFSF(s, s1, id_dest->width);
   rtl_is_sub_overflow(s, s2, s1, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s2);
@@ -44,7 +44,7 @@ static inline def_EHelper(sub) {
   rtl_is_sub_carry(s, s0, ddest, s0);
   rtl_or(s, s0, s0, s2);
   rtl_set_CF(s, s0);
-  printf("%x\n",*s1);
+ // printf("%x\n",*s1);
   //operand_write(s, id_dest, s1);
  //TODO();
 }
