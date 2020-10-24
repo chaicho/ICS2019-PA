34727cd7f844e625632fad8fa24fc17ba0bde4bd
tracer-ics2020 <tracer@njuics.org>
2020-10-24 12:41:54 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  12:41:54 up 17:00,  1 user,  load average: 1.76, 1.35, 1.23

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -34,7 +34,8 @@ static inline def_EHelper(and) {
 static inline def_EHelper(xor) {
   rtl_xor(s,ddest,ddest,dsrc1);
   rtl_update_ZFSF(s,ddest,s->width);
-
+  cpu.eflag.CF=0;
+  cpu.eflag.OF=0;
   print_asm_template2(xor);
 }
 
