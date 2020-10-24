87a550982b6762f839e3192f8286d31cea2351f9
tracer-ics2020 <tracer@njuics.org>
2020-10-24 13:36:48 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  13:36:48 up 17:54,  1 user,  load average: 0.70, 1.06, 1.14

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -41,6 +41,9 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
     case CC_S:
     case CC_L:
     case CC_LE:
+      if(cpu.eflag.ZF==1||cpu.eflag.CF==1) *dest=1;
+      else dest=0;
+      break;
        //TODO();
     default: panic("should not reach here");
     case CC_P: panic("PF is not supported");
