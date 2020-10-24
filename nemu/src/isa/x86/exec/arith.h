6c824b30cf7da8e78b92c19cff5c9b067d071965
tracer-ics2020 <tracer@njuics.org>
2020-10-24 12:52:55 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 6d502a8b740f42ab9f2557872a6c640b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  12:52:55 up 17:11,  1 user,  load average: 1.14, 1.00, 1.09

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -32,7 +32,7 @@ static inline void cmp_internal(DecodeExecState *s) {
 
 static inline def_EHelper(sub) {
   //rtl_sub(s,ddest,ddest,dsrc1);
-  //rtl_get_CF(s, s0);
+  rtl_get_CF(s, s0);
  // rtl_add(s, s0, dsrc1, s0);
  // printf("%x ---- %x\n",*ddest,*dsrc1);
   rtl_sub(s, s1, ddest, dsrc1);
@@ -40,7 +40,7 @@ static inline def_EHelper(sub) {
   rtl_update_ZFSF(s, s1, id_dest->width);
   rtl_is_sub_overflow(s, s2, s1, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s2);
-  //rtl_is_add_carry(s, s2, s0, dsrc1);
+  rtl_is_add_carry(s, s2, s0, dsrc1);
   rtl_is_sub_carry(s, s0, ddest,dsrc1);
   rtl_or(s, s0, s0, s2);
   rtl_set_CF(s, s0);
