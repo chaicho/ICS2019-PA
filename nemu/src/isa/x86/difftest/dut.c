cbd111fb794105c8e8cb48d72678c43169eab3ff
tracer-ics2020 <tracer@njuics.org>
2020-10-25 12:02:55 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: f27f24569ab046acb10f7efc8b2d5d54   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  12:02:55 up 20 min,  1 user,  load average: 1.35, 1.17, 0.77

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -6,33 +6,34 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   // if(ref_r->eflag!=cpu.eflag)printf("The supposed value: %x \n Wrong value : %x\n",ref_r->eflag,cpu.eflag);
   if(ref_r->eax!=cpu.eax) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->eax,cpu.eax);
+    printf("The supposed EAX: %x \n Wrong EAX : %x\n",ref_r->eax,cpu.eax);
     return false;
   }
    if(ref_r->ebp!=cpu.ebp) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->ebp,cpu.ebp);
+    printf("The supposed EBP: %x \n Wrong EBP : %x\n",ref_r->ebp,cpu.ebp);
     return false;
   }
    if(ref_r->ebx!=cpu.ebx) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->ebx,cpu.ebx);
+    printf("The supposed EBX: %x \n Wrong EBX : %x\n",ref_r->ebx,cpu.ebx);
     return false;
   }
    if(ref_r->ecx!=cpu.ecx) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->ecx,cpu.ecx);
+    printf("The supposed ECX: %x \n Wrong ECX : %x\n",ref_r->ecx,cpu.ecx);
     return false;
   }
    if(ref_r->edi!=cpu.edi) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->edi,cpu.edi);
+    printf("The supposed EDI: %x \n Wrong EDI : %x\n",ref_r->edi,cpu.edi);
     return false;
   }
    if(ref_r->edx!=cpu.edx) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->edx,cpu.edx);
+    printf("The supposed EDX: %x \n Wrong EDX : %x\n",ref_r->edx,cpu.edx);
     return false;
   }
   if(ref_r->esp!=cpu.esp) {
-    printf("The supposed value: %x \n Wrong value : %x\n",ref_r->esp,cpu.esp);
+    printf("The supposed ESP: %x \n Wrong ESP : %x\n",ref_r->esp,cpu.esp);
     return false;
   }
+  
   return true;
   // return ref_r->eax==cpu.eax&&ref_r->ebp==cpu.ebp&&ref_r->ebx==cpu.ebx&&ref_r->ecx==cpu.ecx&&ref_r->edi==cpu.edi&&ref_r->edx==cpu.edx&&ref_r->esp==cpu.esp;
 }
