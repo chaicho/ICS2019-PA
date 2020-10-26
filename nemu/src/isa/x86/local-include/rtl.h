b41114dc0de1c40af4f228158929ef497b7db965
tracer-ics2020 <tracer@njuics.org>
2020-10-27 00:44:55 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d500470b355a44449cbf30d370449608   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  00:44:55 up  5:12,  1 user,  load average: 1.81, 1.94, 1.91

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -45,8 +45,7 @@ static inline def_rtl(is_sub_overflow, rtlreg_t* dest,
   // dest <- is_overflow(src1 - src2)
    rtl_msb(s,t0,src1,width);
    rtl_msb(s,t1,src2,width);
-   rtl_sub(s,dest,src1,src2);
-   rtl_msb(s,t2,dest,width); 
+   rtl_msb(s,t2,res,width); 
   //  printf("src1 :  %d   src2: %d  ans: %x  ans_bit : %d\n ",*t0,*t1,*dest,*t2);
    if(*t1==*t0) *dest=0;
    else {
@@ -70,8 +69,7 @@ static inline def_rtl(is_add_overflow, rtlreg_t* dest,
   // dest <- is_overflow(src1 + src2)
    rtl_msb(s,t0,src1,width);
    rtl_msb(s,t1,src2,width);
-   rtl_add(s,dest,src1,src2);
-   rtl_msb(s,t2,dest,width);
+   rtl_msb(s,t2,res,width);
    if(*t1!=*t0) *dest=0;
    else {
       if(*t2==*t0) *dest=0;
