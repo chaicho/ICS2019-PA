df032a4ab16b3ea2986666e5fa5881e35f813a53
tracer-ics2020 <tracer@njuics.org>
2020-10-26 21:35:06 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d500470b355a44449cbf30d370449608   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  21:35:06 up  2:02,  1 user,  load average: 1.99, 1.80, 1.49

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -20,9 +20,9 @@ static inline def_EHelper(add) {
 
 // dest <- sub result
 static inline void cmp_internal(DecodeExecState *s) {
-  rtl_sub(s, s0, dsrc1, ddest);
+  rtl_sub(s, s0, ddest, dsrc1);
   rtl_update_ZFSF(s, s0, id_dest->width);
-  rtl_is_sub_carry(s, s1, dsrc1, ddest);
+  rtl_is_sub_carry(s, s1, ddest, dsrc1);
   rtl_set_CF(s, s1);
   rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
