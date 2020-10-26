5879aa4afb540e2f0868fbe6ee9f09d3b6a184d0
tracer-ics2020 <tracer@njuics.org>
2020-10-26 21:55:07 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d500470b355a44449cbf30d370449608   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  21:55:07 up  2:22,  1 user,  load average: 1.68, 1.52, 1.48

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -47,7 +47,7 @@ static inline def_rtl(is_sub_overflow, rtlreg_t* dest,
    rtl_msb(s,t1,src2,width);
    rtl_sub(s,dest,src1,src2);
    rtl_msb(s,t2,dest,width); 
-   printf("src1 :  %d   src2: %d  ans: %d  ans_bit : %d\n ",*t0,*t1,*dest,*t2);
+   printf("src1 :  %d   src2: %d  ans: %x  ans_bit : %d\n ",*t0,*t1,*dest,*t2);
    if(*t1==*t0) *dest=0;
    else {
       if(*t2!=*t0) *dest=1;
