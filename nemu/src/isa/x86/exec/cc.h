36feb09e0f6ad7bdf2f437ee6bc456da6f5c1821
tracer-ics2020 <tracer@njuics.org>
2020-10-26 21:36:03 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: d500470b355a44449cbf30d370449608   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  21:36:03 up  2:03,  1 user,  load average: 2.03, 1.86, 1.53

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -49,7 +49,7 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
       else *dest =0;
     case CC_LE:
       //TODO();
-      if(cpu.eflag.ZF==1||cpu.eflag.CF!=cpu.eflag.SF) *dest=1;
+      if(cpu.eflag.ZF==1||cpu.eflag.OF!=cpu.eflag.SF) *dest=1;
       else *dest=0;
       break;
        
