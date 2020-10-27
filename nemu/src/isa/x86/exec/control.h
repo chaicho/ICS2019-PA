d07343d24e393cccc329d6c8ddeb2f6d81ac0977
tracer-ics2020 <tracer@njuics.org>
2020-10-28 01:15:49 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 228ab96e5b90415a9f675bafa8382c65   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:15:49 up  1:35,  1 user,  load average: 1.08, 0.74, 0.73

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -46,6 +46,6 @@ static inline def_EHelper(ret_imm) {
 static inline def_EHelper(call_rm) {
   rtl_lm(s,s0,ddest,0,id_dest->width);
   rtl_push(s,s0);
-  rtl_j(s,*s0);
+  rtl_jr(s,s0);
   print_asm("call *%s", id_dest->str);
 }
