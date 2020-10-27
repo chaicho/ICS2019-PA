939bbabd17c13ad4238658c346748770c6d9e14c
tracer-ics2020 <tracer@njuics.org>
2020-10-28 01:34:05 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 228ab96e5b90415a9f675bafa8382c65   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:34:05 up  1:54,  1 user,  load average: 1.31, 1.01, 0.88

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -131,6 +131,7 @@ static inline def_DHelper(mov_G2E) {
  */
 static inline def_DHelper(E2G) {
   operand_rm(s, id_src1, true, id_dest, true);
+ printf("dest: %x  src1: %x\n",*ddest,*dsrc1);
 }
 
 static inline def_DHelper(mov_E2G) {
