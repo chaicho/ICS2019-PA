1e16321cd61a03788d11b5ec76ff3e1d73a4511a
tracer-ics2020 <tracer@njuics.org>
2020-10-27 20:44:07 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: af560a33b95d4f65a2bbfd2e54cdd9c0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:44:07 up 12 min,  1 user,  load average: 1.42, 0.92, 0.56

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -53,6 +53,7 @@ static inline def_EHelper(inc) {
   //TODO();
   rtl_addi(s,s0,ddest,1);
   operand_write(s,id_dest,s0);
+  rtl_set_OF(s,s0);
   rtl_update_ZFSF(s,ddest,id_dest->width);
   print_asm_template1(inc);
 }
@@ -60,6 +61,7 @@ static inline def_EHelper(inc) {
 static inline def_EHelper(dec) {
   rtl_subi(s,s0,ddest,1);
   operand_write(s,id_dest,s0);
+   rtl_set_OF(s,s0);
   rtl_update_ZFSF(s,ddest,id_dest->width);
   print_asm_template1(dec);
 }
