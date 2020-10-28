b33dd8d92c671c8b0c6d5d03ec259f45d4855fd6
tracer-ics2020 <tracer@njuics.org>
2020-10-28 21:53:57 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 306b7646f0644afc835b6bb52584f207   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  21:53:57 up  7:51,  1 user,  load average: 1.79, 1.23, 0.99

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,43 +4,77 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t t=0;
+  while(s[t]!='\0') t++;
+  return t;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  //assert(strlen(dst)>=strlen(src));
+  size_t i;
+  for(i=0;src[i]!='\0';i++) dst[i]=src[i];
+  dst[i]='\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+  for(i=0;i<n&&src[i]!='\0';i++) dst[i]=src[i];
+  for(;i<n;i++) dst[i]='\0';
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t i=0;
+  while (dst[i]!='\0') i++;
+  strcpy(dst+i,src);
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while (*s1==*s2) {
+    if(*(s1)=='\0'||*(s2)=='\0') break;
+    s1++;
+    s2++;
+  }
+  return *(s1)-*(s2);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(0);
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  size_t i=0;
+  unsigned char *s=(unsigned char *)v;
+  for(i=0;i<n;i++){
+    *(s++)=(unsigned char) c;
+  }
+  v=s;
+  return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
+  assert(0);
   return NULL;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  assert(0);
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  return 0;
+  unsigned char *c1=(unsigned char *)s1;
+  unsigned char *c2=(unsigned char *)s2;
+  size_t i=0;
+  while (*c1==*c2) {
+    if(*(c1+i)=='\0'||*(c2+i)=='\0') break;
+    c1++;
+    c2++;
+  }
+  return *c1-*c2;
 }
 
 #endif
