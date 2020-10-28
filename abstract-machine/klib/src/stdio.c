d6a2be51b84daee07e798131058d782d5f91cf4c
tracer-ics2020 <tracer@njuics.org>
2020-10-29 01:14:10 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 306b7646f0644afc835b6bb52584f207   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:14:10 up 11:12,  1 user,  load average: 1.93, 1.62, 1.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,6 +14,49 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list tmp;
+  va_start(tmp,fmt);
+  int i=0;
+  int loc=0;
+  while (fmt[i]!='\0')
+  {
+    if(fmt[i]!='%') {
+      out[loc++]=fmt[i++];
+      continue;
+    }
+    if(fmt[i]=='%'){
+      i++;
+    }
+    switch (fmt[i])
+    {
+    case 's':;
+      char* s=va_arg(tmp,char *);
+      int gg=0;
+      while (s[gg]!='\0')
+      {
+         out[loc++]=s[gg++];
+      }
+      out[loc++]='\0';
+      break;
+    case 'd':;
+      int gg1=va_arg(tmp,int);
+      int store[20],j=-1;
+      while (gg1)
+      {
+          store[++j]=gg1%10;
+          gg1/=10;
+      }
+      for(;j>=0;--j){
+         out[loc++]='0'+store[j];
+      }
+      out[loc++]='\0';
+      break;
+    default:
+      break;
+    }
+    va_end(tmp);
+  }
+  
   return 0;
 }
 
