baf37de9ec56dd47e4bca56fde8ae34031b90680
tracer-ics2020 <tracer@njuics.org>
2020-10-29 15:06:03 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 49a382598a91416e9ad13642fb43a385   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  15:06:03 up 10 min,  1 user,  load average: 0.69, 0.45, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -21,43 +21,44 @@ int sprintf(char *out, const char *fmt, ...) {
   while (fmt[i]!='\0')
   {
     if(fmt[i]!='%') {
-      out[loc++]=fmt[i++];
+      *(out++)=fmt[i++];
       continue;
     }
     if(fmt[i]=='%'){
-      i++;
+        i++;
+        switch (fmt[i++])
+        {
+        case 's':
+        {
+            char *s = va_arg(tmp, char *);
+            int gg = 0;
+            while (s[gg] != '\0')
+            {
+                out[loc++] = s[gg++];
+            }
+             break;
+         }
+        case 'd':;
+         {
+             int gg1=(int)va_arg(tmp,int);
+            int store[20],j=-1;
+            while (gg1)
+            {
+              store[++j]=gg1%10;
+            gg1/=10;
+         }
+            for(;j>=0;--j){
+            *out=(char) ('0'+store[j]);
+            out++;
+             }      
+        break;
+        }
     }
-    switch (fmt[i])
-    {
-    case 's':;
-      char* s=va_arg(tmp,char *);
-      int gg=0;
-      while (s[gg]!='\0')
-      {
-         out[loc++]=s[gg++];
-      }
-      break;
-    case 'd':;
-      int gg1=(int)va_arg(tmp,int);
-      int store[20],j=-1;
-      while (gg1)
-      {
-          store[++j]=gg1%10;
-          gg1/=10;
-      }
-      for(;j>=0;--j){
-         out[loc++]=(char) '0'+store[j];
-      }
-      out[loc++]=gg1+'0';
-      break;
-    default:
-      break;
     }
-    out[loc++]='\0';
-
-    va_end(tmp);
   }
-  
+  *out = '\0';
+
+  va_end(tmp);
   return 0;
 }
 
