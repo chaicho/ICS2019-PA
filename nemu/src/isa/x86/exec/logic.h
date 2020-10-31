b7d06ccbc819f0ef53ebec1950b4d5e542a12ab4
tracer-ics2020 <tracer@njuics.org>
2020-10-31 16:41:58 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: 3beac0db48074fbf940eacf262a5bc73   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  16:41:58 up 18:57,  1 user,  load average: 1.15, 0.99, 0.98

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -46,7 +46,7 @@ static inline def_EHelper(sar) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
   printf("length %d\n",id_dest->width);
-  if(s->isa.is_operand_size_16) *s0=*ddest&0x0000ffff;
+  if(s->isa.is_operand_size_16) rtl_sext(s,s0,ddest,2);
   else *s0=*ddest;
   rtl_sar(s,s0,s0,dsrc1);
   //TODO();
