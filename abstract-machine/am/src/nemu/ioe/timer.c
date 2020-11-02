615ce17a0c259e8aab1748550ccbbf357b956b9b
tracer-ics2020 <tracer@njuics.org>
2020-11-02 22:12:31 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: b568807caf1e419fbf6c9e84a0377ba0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:12:31 up 51 min,  1 user,  load average: 0.53, 0.64, 0.72

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -2,7 +2,7 @@
 #include <nemu.h>
  static uint32_t boot_time=0;
 void __am_timer_init() {
- // boot_time=inl(RTC_ADDR);
+   boot_time=inl(RTC_ADDR+4);
   // asm volatile ("inl %1, %0" : "=a"(boot_time) : "d"(0x48)); 
 }
 
