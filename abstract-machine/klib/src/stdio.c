ecb63a7ec009878083570c4b186d0bb1e3197445
tracer-ics2020 <tracer@njuics.org>
2020-11-02 22:35:11 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: b568807caf1e419fbf6c9e84a0377ba0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:35:11 up  1:13,  1 user,  load average: 0.77, 0.96, 1.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,52 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  
+va_list tmp;
+  va_start(tmp,fmt);
+  int i=0;
+  int gg;
+  int store[32],j=-1;
+  while (fmt[i]!='\0')
+  {
+    if(fmt[i]!='%') {
+     putch(fmt[i++]);
+      continue;
+    }
+    if(fmt[i]=='%'){
+        i++;
+        switch (fmt[i++])
+        {
+        case 's':
+        {
+            char *s = va_arg(tmp, char *);
+            int gg = 0;
+            while (s[gg] != '\0')
+            {
+                putch(s[gg++]);
+            }
+             break;
+         }
+        case 'd':;
+         {
+            gg=(int)va_arg(tmp,int);
+            // int store[20],j=-1;
+            j=-1;
+            while (gg)
+            {
+              store[++j]=gg%10;
+            gg/=10;
+         }
+            for(;j>=0;--j){
+            putch ('0'+store[j]);
+
+             }      
+        break;
+        }
+    }
+    }
+  }
+
+  va_end(tmp);
   return 0;
 }
 
