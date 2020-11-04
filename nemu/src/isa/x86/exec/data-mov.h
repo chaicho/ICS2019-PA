024778b32216fba1342de58f3c437e698edd7e49
tracer-ics2020 <tracer@njuics.org>
2020-11-04 08:44:23 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: f5a08477fbb04f0c8fbd404bf7b90cb5   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  08:44:23 up  1:51,  1 user,  load average: 0.96, 0.79, 0.65

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -79,7 +79,7 @@ static inline def_EHelper(movsx) {
 static inline def_EHelper(movzx) {
   id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
   rtl_zext(s, ddest, dsrc1, id_dest->width);
-  operand_write(s, id_dest, ddest);
+  operand_write(s, id_dest, dsrc1);
   print_asm_template2(movzx);
 }
 
@@ -95,7 +95,7 @@ static inline def_EHelper(movsb) {
     operand_write(s, id_dest, dsrc1); 
  } 
     // *s0=reg_l(R_EDI)+1;
-    cpu.edi+=tt;
+    //cpu.edi+=tt;
     cpu.esi+=tt;
     print_asm_template2(movsb);
 
