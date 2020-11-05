5560c0f00537038aa286b4dd6bb891bc87b8b0c8
tracer-ics2020 <tracer@njuics.org>
2020-11-06 01:23:10 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  01:23:10 up  1:29,  1 user,  load average: 2.03, 2.09, 2.15

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -7,6 +7,6 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //  kbd->keydown = 0;
   //  kbd->keycode = AM_KEY_NONE;
    uint32_t tmp=inl(0xa1000060);
-   kbd->keydown=0;
+   kbd->keydown=(tmp==AM_KEY_NONE)? 0:1;
    kbd->keycode=tmp;
 }
