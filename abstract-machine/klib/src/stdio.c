8231fbcc67b40ec1ada7b933d57d29af270704f2
tracer-ics2020 <tracer@njuics.org>
2020-11-06 19:14:40 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  19:14:40 up 19:20,  1 user,  load average: 2.02, 2.08, 1.95

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -121,10 +121,10 @@ int vsprintf(char *out, const char *fmt, va_list tmp) {
               store[++j]=gg%10;
               gg/=10;
             }
-            while(j<num_before-1){
-              *(ggg++)='0';
-              num_before--;
-            }
+            // while(j<num_before-1){
+            //   *(ggg++)='0';
+            //   num_before--;
+            // }
            for(;j>=0;--j){
              *(ggg++)=(char) ('0'+store[j]);
                }      
