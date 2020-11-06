ff8d58fe9202df847071c8d9b005d1aaaaf8d0fe
tracer-ics2020 <tracer@njuics.org>
2020-11-06 20:36:55 +0800

    >  compile 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:36:55 up 20:43,  1 user,  load average: 3.20, 1.90, 1.54

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -24,7 +24,7 @@ static inline def_EHelper(gp2) {
 
   switch (s->isa.ext_opcode) {
     EXW(0,rol,-1) EMPTY(1) EXW(2,rol,-1) EMPTY(3)
-    EXW(4,shl,-1) EXW(5,shr,-1) EXW(6,shl,1) EXW(7,sar,-1)
+    EXW(4,shl,1) EXW(5,shr,-1) EMPTY(6) EXW(7,sar,-1)
   }
         // printf("NOW:length %d\n",id_dest->width);
 
