3efe21c59436b1e419bc342b40da6aa908640afc
tracer-ics2020 <tracer@njuics.org>
2020-11-06 18:23:11 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  18:23:12 up 18:29,  1 user,  load average: 2.45, 2.27, 2.27

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -1,6 +1,8 @@
 #include <am.h>
 #include <nemu.h>
-
+#include<string.h>
+#define W 400
+#define H 300
  #define SYNC_ADDR (VGACTL_ADDR + 4)
 void __am_gpu_init() {      
     int i;
@@ -21,18 +23,25 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-   int xx=ctl->x;
-   int yy=ctl->y;
-  int ww=ctl->w;
-  int hh=ctl->h;
-  int i,j;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-   for(i=0;i<(hh*4);i++){
-     for(j=0;j<(ww*4);j++){
-         *(fb+xx+yy*400+i+j)=*((uint32_t *) (ctl->pixels+i+j));
-     }
-   }
-  *fb=1;
+  //  int xx=ctl->x;
+  //  int yy=ctl->y;
+  // int ww=ctl->w;
+  // int hh=ctl->h;
+  // int i,j;
+   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  //  for(i=0;i<(hh*4);i++){
+  //    for(j=0;j<(ww*4);j++){
+  //        *(fb+xx+yy*400+i+j)=*((uint32_t *) (ctl->pixels+i+j));
+  //    }
+  //  }
+  // *fb=1;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  int cp_bytes = (W-x>x) ?   sizeof(uint32_t) * w :sizeof(uint32_t) *(W - x);
+  for (int j = 0; j < h && y + j < H; j ++) {
+    memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+    pixels += w;
+  }
   if (ctl->sync) {
     // outl(SYNC_ADDR, 0);
     outl(SYNC_ADDR, 1);
