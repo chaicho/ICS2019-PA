97acdc54760d9ce246d0137a1950389d946a9675
tracer-ics2020 <tracer@njuics.org>
2020-11-06 19:05:40 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  19:05:40 up 19:11,  1 user,  load average: 1.98, 1.71, 1.78

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -2,14 +2,14 @@
 #include <nemu.h>
  static uint32_t boot_time=0;
 void __am_timer_init() {
-   boot_time=inl(RTC_ADDR);
+   boot_time=inl(RTC_ADDR+4);
   // asm volatile ("inl %1, %0" : "=a"(boot_time) : "d"(0x48)); 
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t data;
-  data=inl(RTC_ADDR);    
-  uptime->us=(data-boot_time)*100;
+  data=inl(RTC_ADDR+4);    
+  uptime->us=(data-boot_time)*1000000;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
