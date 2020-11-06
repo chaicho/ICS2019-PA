e27afbe7251fa674112b41ae1f11780e1f375132
tracer-ics2020 <tracer@njuics.org>
2020-11-06 18:02:42 +0800

    >  run 191250127    Static hostname: chaicho          Icon name: computer-laptop            Chassis: laptop         Machine ID: c2a36d1dba684bbab599bf87f794edc9            Boot ID: c4e36774d80c4694a976e7d2cf6393fc   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  18:02:43 up 18:08,  1 user,  load average: 2.19, 2.37, 2.32

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -27,8 +27,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int hh=ctl->h;
   int i,j;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-   for(i=0;i<hh;i++){
-     for(j=0;j<ww;j++){
+   for(i=0;i<(hh*4);i++){
+     for(j=0;j<(ww*4);j++){
          *(fb+xx+yy*400+i+j)=*((uint32_t *) (ctl->pixels+i+j));
      }
    }
